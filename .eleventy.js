7f39feb6bcdda3f79f019bf1ab17ce14f57f97fc
TANIGUCHI Masaya <mew@cat.ovh>
2020-04-25 11:58:38 +0900

    Use markdown-it-container

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,23 @@
 const markdownIt = require('markdown-it')
+const markdownItContainer = require('markdown-it-container')
 const markdownItMathjax3 = require('markdown-it-mathjax3')
 const markdownItPrism = require('markdown-it-prism')
 const htmlMinifier = require('html-minifier')
 
-module.exports = (eleventyConfig) => {
-    eleventyConfig.addPassthroughCopy("CNAME")
-    eleventyConfig.setLibrary("md", markdownIt().use(markdownItMathjax3).use(markdownItPrism))
-    eleventyConfig.addTransform("html-minifier", (content, outputPath) => {
+const markdown =
+    markdownIt()
+        .use(markdownItMathjax3)
+        .use(markdownItPrism)
+        .use(markdownItContainer, 'proof')
+        .use(markdownItContainer, 'theorem')
+        .use(markdownItContainer, 'lemma')
+        .use(markdownItContainer, 'definition')
+        .use(markdownItContainer, 'axiom')
+        .use(markdownItContainer, 'example')
+        .use(markdownItContainer, 'remark')
+
+const minify =
+    (content, outputPath) => {
         if (outputPath.endsWith(".html")) {
             let minified = htmlMinifier.minify(content, {
                 removeComments: true,
@@ -17,5 +28,10 @@ module.exports = (eleventyConfig) => {
             return minified;
         }
         return content;
-    });
+    }
+
+module.exports = (eleventyConfig) => {
+    eleventyConfig.addPassthroughCopy("CNAME")
+    eleventyConfig.setLibrary("md", markdown)
+    eleventyConfig.addTransform("html-minifier", minify);
 }
