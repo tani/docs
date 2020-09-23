5377db1f9b611a4b34004489042d2b6df56f83ba
TANIGUCHI Masaya <mew@cat.ovh>
2020-09-23 15:25:21 +0900

    Load a stylesheet asynchronously

--- template.js
+++ template.js
@@ -10,7 +10,7 @@ ${doctype}
         <title>${frontmatter.title} - www.docs.casa</title>
         <link rel="icon" href="data:," />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Noto+Sans+SC:wght@400;700&family=Source+Code+Pro&family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Noto+Sans+SC:wght@400;700&family=Source+Code+Pro&family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" media="print" onload="this.media='all'" />
         <style>
         ${fs.readFileSync("node_modules/picnic/picnic.min.css", "utf-8")}
         ${fs.readFileSync("node_modules/highlight.js/styles/default.css", "utf-8")}
